78f2342c4b27f5a5b10f9a71c8b46b98ab7287ca
FF <f+git@fida.biz>
2017-06-30 23:14:15 +0200

    StringReducer

---
+++ Helper/StringReducer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace MonoFcgiTest.Helper
+{
+    public static class StringReducer
+    {
+        public static string RemoveWhitespace(this string s) => Regex.Replace(s, @"\s+", "");
+
+        public static string CamelCase(this string @string)
+        {
+            return Regex.Split(@string, @"\s")
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .ToCamelCaseCollection()
+                .ConcatCollection();
+        }
+
+        private static IEnumerable<string> ToCamelCaseCollection(this IEnumerable<string> @string)
+            => @string.Select(FirstToUppercase);
+
+        private static string FirstToUppercase(this string @string)
+        {
+            var sb = new StringBuilder();
+            if (@string.Length > 0)
+            {
+                sb.Append(char.ToUpper(@string[0]));
+            }
+            if (@string.Length > 1)
+            {
+                sb.Append(@string.Substring(1));
+            }
+            return sb.ToString();
+        }
+
+        private static string ConcatCollection(this IEnumerable<string> strings) => string.Join("", strings);
+
+       
+    }
+
+
+}
