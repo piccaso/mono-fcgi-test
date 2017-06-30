64a6b7fa176504c3364d15a787cfefb81ff91fa3
FF <f+git@fida.biz>
2017-06-30 23:29:37 +0200

    StringReducingService

---
+++ IStringReducingService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MonoFcgiTest
+{
+    [ServiceContract]
+    public interface IStringReducingService
+    {
+        [OperationContract]
+        string Trim(string @string);
+
+        [OperationContract]
+        string RemoveWhitespace(string @string);
+
+        [OperationContract]
+        string CamelCase(string @string);
+    }
+}
---
+++ StringReducingService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using MonoFcgiTest.Helper;
+
+namespace MonoFcgiTest
+{
+    public class StringReducingService : IStringReducingService
+    {
+
+        public string Trim(string @string) => @string.Trim();
+        public string RemoveWhitespace(string @string) => @string.RemoveWhitespace();
+        public string CamelCase(string @string) => @string.CamelCase();
+    }
+}
